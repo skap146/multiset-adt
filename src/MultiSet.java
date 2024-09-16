51cface03d717d16e78112d802a6add3b9660745
skapc146 <skapcan52@gmail.com>
2024-09-16 12:18:32 -0400

    Added abstract methods for MultiSet abstract data class

--- src/MultiSet.java
+++ src/MultiSet.java
@@ -10,5 +10,15 @@ public abstract class MultiSet {
      */
     abstract void add(Integer item);
 
+    abstract void remove(Integer item);
+
+    abstract boolean contains(Integer item);
+
+    abstract boolean is_empty();
+
+    abstract int count();
+
+    abstract int size(Integer item);
+
     // TODO finish adding abstract methods to fully describe what it means to be a MultiSet.
 }
